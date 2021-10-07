3cb35f3f177f404efff114efae1874a24c40c4ae
Grigoriy Shoroshov <shoroh2525@gmail.com>
2021-10-07 17:44:45 +0300

    Add minimal solution for 03_Multifile

---
+++ 03_Multifile/prog.c
@@ -0,0 +1,14 @@
+#include "outlib.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int i;
+    if ((Count = argc) > 1) {
+        output("<INIT>");
+        for (i = 1; i < argc; i++)
+            output(argv[i]);
+        output("<DONE>");
+    } else
+        usage(argv[0]);
+    return 0;
+}
