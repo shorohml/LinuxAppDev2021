44e104c0aa943224060849cdd5d1c1bad61f101f
shorohml <shoroh2525@gmail.com>
2021-12-25 22:32:21 +0300

    Add translation with gettex

--- 10_I18n/src/guess.c
+++ 10_I18n/src/guess.c
@@ -1,36 +1,49 @@
+#include <ctype.h>
+#include <libgen.h>
+#include <libintl.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-#include <math.h>
+
+#define UPPER 100
+
+#define _(STRING) gettext(STRING)
+#define LOCALE_PATH "."
 
 char *to_lower(char *s) {
     for (char *p = s; *p; ++p) {
-        *p  = tolower(*p);
+        *p = tolower(*p);
     }
     return s;
 }
 
-int main() {
-    int lower = 1, upper = 100, half;
+int main(int argc, char **argv) {
+    int lower = 1, upper = UPPER, half;
     size_t n;
-    char *answer = NULL;
+    char *answer = NULL, *dir;
 
-    printf("Guess a number from 1 to 100\n");
+    dir = dirname(realpath(argv[0], NULL));
+    setlocale(LC_ALL, "");
+    bindtextdomain("guess", LOCALE_PATH);
+    textdomain("guess");
+
+    printf(_("Guess a number from 1 to %d\n"), UPPER);
     while (lower < upper - 1) {
         half = (lower + upper) / 2;
-        printf("is the number greater than %d? (y/n)\n", half);
+        printf(_("Is the number greater than %d? (y/n)\n"), half);
+
         getline(&answer, &n, stdin);
         answer = to_lower(answer);
 
-        if (!strcmp(answer, "y\n") || !strcmp(answer, "yes\n")) {
+        if (!strcmp(answer, _("y\n")) || !strcmp(answer, _("yes\n"))) {
             lower = half;
-        } else if (!strcmp(answer, "n\n") || !strcmp(answer, "no\n")) {
+        } else if (!strcmp(answer, _("n\n")) || !strcmp(answer, _("no\n"))) {
             upper = half;
         }
 
         free(answer);
         answer = NULL;
     }
-    printf("Your number is %d\n", upper);
+    printf(_("Your number is %d\n"), upper);
 }
